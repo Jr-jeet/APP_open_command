24cfe3e95f366dc403c24f71e39207f28a9d8bab
Jr-jeet <jeet62032@gmail.com>
2026-08-08 23:21:24 +0530

    Clean up AI.py and import pandas
    Removed commented-out code for reading files and added import for pandas.

--- AI.py
+++ AI.py
@@ -15,6 +15,15 @@ def open_app(command):
 open_app(command)
 """  """
 
+import pandas as p 
+# df=p.read_csv("himani  sem 2 addmission.pdf",encoding=latin1 / utf-8)
+#(if error come then:-  then pass in encoding , utf-8  or latin1 )
+# df=p.read_excel("himani  sem 2 addmission.pdf",encoding=latin1 / utf-8)
+# df=p.read_json("himani  sem 2 addmission.pdf",encoding=latin1 / utf-8)
+# print(df)              use for read data "what is in the data"
+# gcsfs   if data in google cloude
+
+# data ={
 
 
     
